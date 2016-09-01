f49269a2994a5fbd6c771fedd9694d165f4841e5
Alanc Liu <alanc.liu@btcc.com>
2016-09-01 12:18:17 +0800

    first commit

---
+++ TestApp/ViewController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using UIKit;
+using Foundation;
+
+namespace TestApp
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+			this.View.BackgroundColor = UIColor.Red;
+
+			this.View.AddGestureRecognizer (new UITapGestureRecognizer (() => {
+				if(!UIApplication.SharedApplication.OpenUrl(NSUrl.FromString("testscheme://com.companyname.testinvokeapp?HelloWorld!")))
+				{
+					Console.WriteLine("app is not exist!");
+				}
+			}));
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
